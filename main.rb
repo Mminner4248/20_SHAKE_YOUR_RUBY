ef92f490093c874f0c7426016510f812b149811d
Matt Minner <matthewfminner@gmail.com>
2017-10-18 14:30:41 -0500

    finished

---
+++ main.rb
@@ -0,0 +1,158 @@
+flowers = ["Everybody clap", "Moonwalk", "Shopping Cart", "Robot", "Twist", "Cabbage Patch", "Raise the roof", "Sprinkler", "Running Man", "Worm", "Shake your Ruby"]
+
+e = flowers.to_enum
+
+3.times do 
+    puts e.next
+end
+
+2.times do
+    e.rewind.next
+end
+
+puts e.next
+
+puts e.rewind.next
+
+2.times do
+    e.next
+end
+
+puts e.next
+puts e.next
+
+e.rewind
+
+3.times do 
+    e.next
+end
+
+puts e.next
+
+puts e.rewind.next
+
+9.times do 
+    e.next
+end
+
+puts e.next
+
+puts e.rewind.next
+
+6.times do
+    e.next
+end
+
+puts e.next
+
+e.rewind
+
+6.times do
+    e.next
+end
+
+puts e.next
+
+e.next
+
+puts e.next
+
+e.rewind
+
+5.times do
+    e.next
+end
+
+puts e.next
+
+3.times do
+    e.next
+end
+
+puts e.next
+
+puts e.rewind.next
+
+9.times do 
+    e.next
+end
+
+puts e.next
+
+puts e.rewind.next
+
+puts e.next
+
+7.times do
+    e.next
+end
+
+puts e.next
+
+e.rewind
+
+3.times do
+    e.next
+end
+
+puts e.next
+
+3.times do
+    e.next
+end
+
+puts e.next
+
+puts e.rewind.next
+
+9.times do 
+    e.next
+end
+
+puts e.next
+
+puts e.rewind.next
+
+e.next
+
+puts e.next
+
+5.times do
+    e.next
+end
+
+puts e.next
+
+e.rewind
+
+6.times do
+    e.next
+end
+
+puts e.next
+
+e.rewind
+
+4.times do
+    e.next
+end
+
+puts e.next
+
+5.times do
+    e.next
+end
+
+puts e.next
+
+puts e.rewind.next
+
+9.times do 
+    e.next
+end
+
+puts e.next
+
+
+
+
